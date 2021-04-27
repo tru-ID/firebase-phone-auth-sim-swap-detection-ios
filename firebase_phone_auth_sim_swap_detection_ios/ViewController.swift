b4bc92e98d4cf99e2120bb0dfbd8c4560b99407a
Didem Yakici <didematilla@gmail.com>
2021-04-27 10:55:24 +0100

    Creating initial iOS Project

---
+++ firebase_phone_auth_sim_swap_detection_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firebase_phone_auth_sim_swap_detection_ios
+//
+//  Created by Didem Yakici on 27/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
